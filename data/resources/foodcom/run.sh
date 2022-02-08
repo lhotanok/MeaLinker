2cf783f30780d8cad3ca2490a6ca727d547d242f
Kristýna Lhoťanová <kristyna.lhotanova@gmail.com>
2022-02-08 23:37:29 +0100

    Add run script compatible with Linux system

---
+++ data/resources/foodcom/run.sh
@@ -0,0 +1,5 @@
+node recipe-urls-generator.js
+node recipes-ingredients-merge-manager.js
+node rdf-ingredients-converter.js
+node rdf-data/external-dataset-linker.js
+node ingredients-merge-manager.js
