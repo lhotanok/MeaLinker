0220a969505320c09461ad2c150f509cf8ac21c8
Kristýna Lhoťanová <kristyna.lhotanova@gmail.com>
2022-03-08 00:47:34 +0100

    Add linear loading progress component

---
+++ app/frontend/src/shared/components/LoadingProgress.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import LinearProgress from '@mui/material/LinearProgress';
+import Box from '@mui/material/Box';
+
+export default function LoadingProgress() {
+  return (
+    <Box sx={{ width: '95%', margin: 'auto' }}>
+      <LinearProgress color='secondary' />
+    </Box>
+  );
+}
